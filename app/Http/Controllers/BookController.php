a572a2045c5b60c3e93841be2b2bf7d3f845ca41
Dams168 <adamrohman2003@gmail.com>
2024-11-21 22:31:53 +0700

    add: P6 read and create

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Bookshelf;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $data['books'] = Book::all();
+        return view('books.index', $data);
+    }
+    public function create()
+    {
+        $data['bookshelves'] = Bookshelf::pluck('name', 'id');
+        return view('books.create', $data);
+    }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'author' => 'required|max:255',
+            'year' => 'required|max:2077',
+            'publisher' => 'required|max:255',
+            'city' => 'required|max:50',
+            'cover' => 'required',
+            'bookshelf_id' => 'required|max:5',
+        ]);
+        if ($request->hasFile('cover')) {
+            $path = $request->file('cover')->storeAs(
+                'public/cover_buku',
+                'cover_buku_' . time() . '.' . $request->file('cover')->extension()
+            );
+            $validated['cover'] = basename($path);
+        }
+        $book = Book::create($validated);
+        if ($book) {
+            $notification = array(
+                'message' => 'Data buku berhasil disimpan',
+                'alert-type' => 'success'
+            );
+        } else {
+            $notification = array(
+                'message' => 'Data buku gagal disimpan',
+                'alert-type' => 'success'
+            );
+        }
+        return redirect()->route('book')->with($notification);
+    }
+}
